be4125785394b54a1f2e99466828b42e7a5964c3
aaswin <arjundhakal961@gmail.com>
2020-07-22 12:01:25 +0545

    Updates

---
+++ precache-manifest.018ce5d57d5ff6b0edaef37689e38d3a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1906244af1fc875a942d87bbfc6de8d7",
+    "url": "/thegrandmedia/index.html"
+  },
+  {
+    "revision": "92321ad160081ea29269",
+    "url": "/thegrandmedia/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5b3a4fe27be2daa3fd4a",
+    "url": "/thegrandmedia/static/js/2.b06613dd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/thegrandmedia/static/js/2.b06613dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92321ad160081ea29269",
+    "url": "/thegrandmedia/static/js/main.d5fc7e83.chunk.js"
+  },
+  {
+    "revision": "25e6ab0e8f3173221dc9",
+    "url": "/thegrandmedia/static/js/runtime-main.b9cbe80e.js"
+  },
+  {
+    "revision": "e8cf3c4f0b2bd34c9302a57d221993a0",
+    "url": "/thegrandmedia/static/media/allInfo2.e8cf3c4f.jpg"
+  },
+  {
+    "revision": "29ce977cc7fdf1ac52fc74bb3ad2a8ec",
+    "url": "/thegrandmedia/static/media/effect.29ce977c.png"
+  },
+  {
+    "revision": "43050d1b3d5267089926e354ca275954",
+    "url": "/thegrandmedia/static/media/feed.43050d1b.webp"
+  },
+  {
+    "revision": "0dd0d8d7224eebd27c69b8d291c54d16",
+    "url": "/thegrandmedia/static/media/filter.0dd0d8d7.jpg"
+  },
+  {
+    "revision": "2201ed862faa34651a668e07d0072deb",
+    "url": "/thegrandmedia/static/media/heart.2201ed86.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/thegrandmedia/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "38f8ec063304d40c7796527ccda7c57d",
+    "url": "/thegrandmedia/static/media/notify.38f8ec06.jpg"
+  },
+  {
+    "revision": "42739f1d3d58d1be00d3a5c626a45cc3",
+    "url": "/thegrandmedia/static/media/profile.42739f1d.jpg"
+  },
+  {
+    "revision": "779e96e71eb18c0628b463d9017cf957",
+    "url": "/thegrandmedia/static/media/signuptk.779e96e7.png"
+  },
+  {
+    "revision": "851a9b3effa64a7e713374b3482dccef",
+    "url": "/thegrandmedia/static/media/signwithNo.851a9b3e.png"
+  }
+]);
